db0ab162284a92f5a7a624763544257ca67caea4
MikhailKr <kravchenko.mi@citilink.ru>
2018-08-09 18:21:42 +0300

    Добавил кнопку вперёд

---
+++ src/components/buttons/nextBtn.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './nextBtn.css';
+
+const NextBtn = ({name}) => (
+    <button className={'next-btn'}>
+        {name}
+    </button>
+)
+
+export default NextBtn;
